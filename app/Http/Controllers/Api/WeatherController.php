88053c9e098017b02296de9b85cfbbff1687b657
PovilasKorop <povilask007@gmail.com>
2021-10-24 21:04:27 +0300

    Fix API call

--- app/Http/Controllers/Api/WeatherController.php
+++ app/Http/Controllers/Api/WeatherController.php
@@ -13,7 +13,7 @@ class WeatherController extends Controller
         $coordinates = config('app.cities.'.$city);
 
         return Cache::remember('city' . $city, 60 * 5, function() use ($coordinates) {
-            $response = Http::get('https://api.open-meteo.com/vsdfsd1/forecast?latitude='.$coordinates['lat'].'&longitude='.$coordinates['lng'].'&daily=temperature_2m_max,temperature_2m_min&timezone=UTC');
+            $response = Http::get('https://api.open-meteo.com/v1/forecast?latitude='.$coordinates['lat'].'&longitude='.$coordinates['lng'].'&daily=temperature_2m_max,temperature_2m_min&timezone=UTC');
 
             if ($response->successful()) {
                 return $response->json('daily');
